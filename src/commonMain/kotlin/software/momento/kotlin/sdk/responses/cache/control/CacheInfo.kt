4f82d75dc387d157be123b4339cdd908e83ba279
Pratik Agarwal <pratik@momentohq.com>
2024-01-08 10:51:54 -0800

    chore: refactoring comments

--- src/commonMain/kotlin/software/momento/kotlin/sdk/responses/cache/control/CacheInfo.kt
+++ src/commonMain/kotlin/software/momento/kotlin/sdk/responses/cache/control/CacheInfo.kt
@@ -1,11 +1,6 @@
 package software.momento.kotlin.sdk.responses.cache.control
+
 /** Information about a cache.
  *  @param name the cache name
  * */
-public data class CacheInfo
-
-/**
- * Constructs a CacheInfo.
- *
- * @param name the name of the cache.
- */(public val name: String)
+public data class CacheInfo(public val name: String)
